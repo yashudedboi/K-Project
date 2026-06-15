edb5ed4c124a8096c4cdb7854ef9293e15959aa9
yashudedboi <aaravb2@live.wsd1.org>
2026-06-14 22:12:22 -0500

    Add gun sfx if possible and add a Main Menu

--- Assets/Scripts/CheckPoint.cs
+++ Assets/Scripts/CheckPoint.cs
@@ -1,13 +1,14 @@
 using UnityEngine;
 using UnityEngine.UI;
 
+// This guarantees an AudioSource will always exist on this GameObject
+[RequireComponent(typeof(AudioSource))]
 public class CheckPoint : MonoBehaviour
 {
     [SerializeField] GameObject player;
     [SerializeField] Vector3 vectorPoint;
 
     [Header("Spawn Settings")]
-    // This will lift the player up on the Y axis so they don't spawn inside the box
     [SerializeField] Vector3 spawnOffset = new Vector3(0, 1.5f, 0);
 
     public int health = 100;
@@ -16,28 +17,39 @@ public class CheckPoint : MonoBehaviour
 
     private Rigidbody playerRb;
 
+    private AudioSource soundPlayer; // Made private since we get it automatically
+    public AudioClip Level1;
+    public AudioClip Level2;
+    public AudioClip Level3;
+
     public void Start()
     {
+        // FIX: Correctly assign the AudioSource component to the variable
+        soundPlayer = GetComponent<AudioSource>();
+
         isPlayerDead = false;
         if (healthSlider != null)
         {
             healthSlider.maxValue = health;
             healthSlider.value = health;
         }
-        vectorPoint = player.transform.position;
-        playerRb = player.GetComponent<Rigidbody>();
+
+        if (player != null)
+        {
+            vectorPoint = player.transform.position;
+            playerRb = player.GetComponent<Rigidbody>();
+        }
     }
 
     void Update()
     {
         if (isPlayerDead)
         {
-            // Teleport to the checkpoint PLUS the safety offset
             player.transform.position = vectorPoint + spawnOffset;
 
             if (playerRb != null)
             {
-                playerRb.linearVelocity = Vector3.zero; // Use playerRb.velocity on older Unity versions
+                playerRb.linearVelocity = Vector3.zero;
                 playerRb.angularVelocity = Vector3.zero;
             }
 
@@ -50,45 +62,33 @@ public class CheckPoint : MonoBehaviour
 
     private void OnTriggerEnter(Collider other)
     {
-        if (other.gameObject.CompareTag("CheckPoint"))
+        if (other.gameObject.CompareTag("CheckPoint1"))
         {
-            // Save the base position of the checkpoint box
             vectorPoint = other.transform.position;
+            soundPlayer.clip = Level1;
+            soundPlayer.Play();
+            Debug.Log("Checkpoint Saved!");
+        }
+
+        if (other.gameObject.CompareTag("CheckPoint2"))
+        {
+            vectorPoint = other.transform.position;
+            soundPlayer.clip = Level2;
+            soundPlayer.Play();
+            Debug.Log("Checkpoint Saved!");
+        }
+
+        if (other.gameObject.CompareTag("CheckPoint3"))
+        {
+            vectorPoint = other.transform.position;
+            soundPlayer.clip = Level3;
+            soundPlayer.Play();
             Debug.Log("Checkpoint Saved!");
         }
 
         if (other.gameObject.CompareTag("Lava"))
         {
             PlayerDied();
-        }
-    }
-
-    private void OnCollisionEnter(Collision collision)
-    {
-        if (collision.gameObject.CompareTag("CheckPoint"))
-        {
-            // Save the base position of the checkpoint box
-            vectorPoint = collision.transform.position;
-            Debug.Log("Checkpoint Saved!");
-        }
-
-        if (collision.gameObject.CompareTag("Lava"))
-        {
-            PlayerDied();
-        }
-
-        if (collision.gameObject.CompareTag("Projectile"))
-        {
-            Destroy(collision.gameObject);
-            if (health > 10)
-            {
-                health -= 10;
-                if (healthSlider != null) healthSlider.value = health;
-            }
-            else
-            {
-                PlayerDied();
-            }
         }
     }
 
